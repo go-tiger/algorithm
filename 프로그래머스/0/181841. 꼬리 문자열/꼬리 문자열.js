a8992488213308f964991112e3a26653e86a24c1
고랑 <inter_tiger_@naver.com>
2024-06-25 15:11:43 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.js
@@ -0,0 +1,9 @@
+function solution(str_list, ex) {
+    var answer = '';
+    for (let i = 0; i < str_list.length; i++) {
+        if (!str_list[i].includes(ex)) {
+            answer += str_list[i];
+        }
+    }
+    return answer;
+}
