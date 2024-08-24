59383d849a49ec5512ebabb5301cb4bd46838e7a
고랑 <inter_tiger_@naver.com>
2024-08-24 20:27:02 +0900

    [level 0] Title: 배열 만들기 4, Time: 3.93 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+    let stk = [];
+    let i = 0;
+    while (i < arr.length) {
+        if (stk.length === 0 || stk[stk.length - 1] < arr[i]) {
+            stk.push(arr[i]);
+            i++;
+        } else if (stk[stk.length - 1] >= arr[i]) {
+            stk.pop();
+        }
+    }
+    return stk;
+}
