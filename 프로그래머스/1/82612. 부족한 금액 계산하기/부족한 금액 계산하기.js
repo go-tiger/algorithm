2eb9e94eb0a02dcd1a6057f880a4091277edc0c6
고랑 <inter_tiger_@naver.com>
2024-10-16 23:15:39 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,8 @@
+function solution(price, money, count) {
+    let answer = 0
+
+    for (let i = 1; i <= count; i++) {
+        answer += price * i
+    }
+    return answer > money ? answer - money : 0
+}
