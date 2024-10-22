d6703c6cc3ae4218c7e2d3239a005521775cd4cf
고랑 <inter_tiger_@naver.com>
2024-10-22 16:25:11 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.79 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    let answer = [];
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            answer.push(numbers[i] + numbers[j])
+        }
+    }
+    answer.sort((a, b) => a - b)
+    
+    return [...new Set(answer)]
+}
