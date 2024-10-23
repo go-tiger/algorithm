95f9983e97844e9eae6a8292107a573d15cd168d
고랑 <inter_tiger_@naver.com>
2024-10-23 11:56:23 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.85 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    const answer = new Array(s.length).fill(-1);
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        for (let j = i - 1; j >= 0; j--) {
+            if (s[i] === s[j]) {
+                answer[i] = i - j
+                break
+            };
+        }
+    }
+    return answer;
+}
