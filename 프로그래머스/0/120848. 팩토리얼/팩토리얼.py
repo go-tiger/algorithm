d9507250c8581bdff2344dc2e1022917afda38b6
고랑 <inter_tiger_@naver.com>
2025-08-02 18:46:37 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+        if factorial <= n:
+            answer = i
+        else:
+            break
+    return answer
