c5c8bf769ffc17d74ea8a1eaecea9c291180b244
고랑 <inter_tiger_@naver.com>
2025-11-20 21:05:49 +0900

    [level 1] Title: 폰켓몬, Time: 27.92 ms, Memory: 34.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,13 @@
+function solution(nums) {
+    let answer = []
+    let max = nums.length / 2
+        
+    for (let i = 0; i < nums.length; i++) {
+        if (answer.length < max) {
+            if (!answer.includes(nums[i])) {
+                answer.push(nums[i])
+            }
+        }
+    }
+    return answer.length;
+}
